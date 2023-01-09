b5711981d0b4a63bd3dd61aa5e390c041cccf332
Given Ncube <givenyslim12@gmail.com>
2023-01-10 01:28:28 +0200

    feat: added the ability get permission names for a given model - internal methods now use the facade to get permissions to generate - can be used in user land to permission for a given model

--- src/LaravelAuthorizer.php
+++ src/LaravelAuthorizer.php
@@ -2,6 +2,14 @@
 
 namespace FlixtechsLabs\LaravelAuthorizer;
 
+use Illuminate\Support\Str;
+
 class LaravelAuthorizer
 {
+    public function getPermissionsFor(string $model): array
+    {
+        return array_map(static function (string $permission) use ($model) {
+            return $permission.' '.(Str::contains($permission, 'all') ? Str::of($model)->snake()->plural()->lower() : Str::of($model)->snake()->lower());
+        }, config('authorizer.permissions'));
+    }
 }
